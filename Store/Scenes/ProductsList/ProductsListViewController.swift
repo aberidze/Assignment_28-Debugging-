94d1a962dc44ebdcc1a96e353981d97d3d16ecd6
Anri_Beridze <aberidze02@gmail.com>
2023-11-27 09:34:17 +0400

    several errors fixed

---
+++ Store/Scenes/ProductsList/ProductsListViewController.swift
@@ -0,0 +1,147 @@
+//
+//  ProductsListViewController.swift
+//  Store
+//
+//  Created by Baramidze on 25.11.23.
+//
+
+import UIKit
+
+class ProductsListViewController: UIViewController {
+    
+    private let productsTableView: UITableView = {
+       let tableView = UITableView()
+        tableView.backgroundColor = .purple
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    private var activityIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView()
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        indicator.hidesWhenStopped = true
+        indicator.style = .large
+        indicator.color = .white
+        return indicator
+    }()
+    
+    private let totalPriceLbl: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "total: 0$"
+        label.textColor = .red
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let productsViewModel = ProductsListViewModel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupProductsViewModel()
+        productsViewModel.viewDidLoad()
+        activityIndicator.startAnimating()
+    }
+    
+    //MARK: setup UI
+    func setupUI() {
+        view.backgroundColor = .orange
+        setupTableView()
+        setupIndicator()
+        setupTotalPriceLbl()
+    }
+    
+    func setupTableView() {
+        view.addSubview(productsTableView)
+        
+        NSLayoutConstraint.activate([
+            productsTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            productsTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
+            productsTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            productsTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        productsTableView.register(UINib(nibName: "ProductCell", bundle: nil), forCellReuseIdentifier: "ProductCell")
+        // FIXME: no delegate and dataSource assigned to productsTableView
+        productsTableView.dataSource = self
+        productsTableView.delegate = self
+    }
+    
+    func setupIndicator() {
+        
+        view.addSubview(activityIndicator)
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    func setupTotalPriceLbl() {
+        view.addSubview(totalPriceLbl)
+        
+        NSLayoutConstraint.activate([
+            totalPriceLbl.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            totalPriceLbl.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            totalPriceLbl.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    //MARK: Setup delegates
+    private func setupProductsViewModel() {
+        productsViewModel.delegate = self
+    }
+}
+
+extension ProductsListViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let currentProduct = productsViewModel.products?[indexPath.row],
+              // FIXME: tableView used instead of productsTableView
+              let cell = productsTableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath) as? ProductCell
+        else {
+            return UITableViewCell()
+        }
+        cell.delegate = self
+        cell.reload(with: currentProduct)
+        return cell
+    }
+}
+
+extension ProductsListViewController: ProductsListViewModelDelegate {
+    
+    func productsAmountChanged() {
+        totalPriceLbl.text = "Total price: \(productsViewModel.totalPrice ?? 0)"
+    }
+    
+    func productsFetched() {
+        // FIXME: must be called on a mean thread
+        DispatchQueue.main.async {
+            self.activityIndicator.stopAnimating()
+            self.productsTableView.reloadData()
+        }
+    }
+}
+
+extension ProductsListViewController: ProductCellDelegate {
+    
+    func removeProduct(for cell: ProductCell) {
+        if let indexPath = productsTableView.indexPath(for: cell) {
+            productsViewModel.removeProduct(at: indexPath.row + 1)
+        }
+    }
+    
+    func addProduct(for cell: ProductCell) {
+        if let indexPath = productsTableView.indexPath(for: cell) {
+            productsViewModel.addProduct(at: indexPath.row + 1)
+        }
+    }
+}
+
+
